32f23f46b56f75de650359e496829e30815006bf
Ben <benni-vi@gmx.net>
2012-06-04 14:28:47 +0200

    Added first frontend-files. Open calendar_monthview.xml to see first example.

---
+++ Yet-Another-Calendar/src/main/webapp/Resources/analog_clock.js
@@ -0,0 +1,101 @@
+function zeichnen() {
+	var objCanvas = document.getElementById('analog_clock');
+	AnalogClock(objCanvas);
+}
+
+window.onload = function() {
+	zeichnen();
+}
+
+function AnalogClock(objCanvas) {
+
+	// Neues Datumsobjekt
+	var objDate = new Date();
+	var intSek = objDate.getSeconds(); // Sekunden 0..59
+	var intMin = objDate.getMinutes(); // Minuten 0..59
+	var intHours = objDate.getHours() % 12; // Stunden 0..11
+	// Kontext-Objekt
+	var objContext = objCanvas.getContext("2d");
+
+	objContext.clearRect(0, 0, 150, 150); // Anzeigebereich leeren
+
+	objContext.save(); // Ausgangszustand speichern
+	objContext.translate(75, 75); // Koordinatensystem in Mittelpkt des
+									// Ziffernblatts verschieben
+
+	objContext.save();
+	objContext.beginPath();
+	objContext.arc(0, 0, 75, 0, 2 * Math.PI, false);
+	objContext.fillStyle = "#000";
+	objContext.fill();
+	objContext.restore();
+
+	objContext.save();
+	objContext.beginPath();
+	objContext.arc(0, 0, 65, 0, 2 * Math.PI, false);
+	objContext.fillStyle = "#fff";
+	objContext.fill();
+	objContext.restore();
+
+	// Minuten
+	objContext.save();
+	for ( var i = 0; i < 60; i++) {
+		objContext.beginPath();
+		if (i % 5 == 0) {
+			objContext.moveTo(0, -45);
+			objContext.lineTo(0, -60);
+			objContext.lineWidth = 3;
+		} else {
+			objContext.moveTo(0, -53);
+			objContext.lineTo(0, -60);
+			objContext.lineWidth = 1;
+		}
+		objContext.strokeStyle = "#000";
+		objContext.stroke();
+		objContext.rotate(Math.PI / 30);
+	}
+	objContext.restore();
+
+	// Stunden
+	objContext.save();
+	// Aktuelle Stunde zzgl. Minutenanteil über Drehung des Koordinatensystems
+	// (kontinuierlicher Übergang zwischen zwei Stunden gewünscht, keine Sprung)
+	objContext.rotate(intHours * Math.PI / 6 + intMin * Math.PI / 360);
+	objContext.beginPath(); // Neuen Pfad anlegen
+	objContext.moveTo(0, 10); // Zeiger über Mitte hinaus zeichnen
+	objContext.lineTo(0, -38); // Stundenzeiger im gedrehten Koord-Sys. um 38
+								// Einheiten nach oben zeichnen
+	// Linienstyle festlegen und zeichnen
+	objContext.lineWidth = 4;
+	objContext.strokeStyle = "#666";
+	objContext.stroke();
+	objContext.restore();
+
+	// Minuten
+	objContext.save();
+	objContext.rotate(intMin * Math.PI / 30);
+	objContext.beginPath();
+	objContext.moveTo(0, 10);
+	objContext.lineTo(0, -50);
+	objContext.lineWidth = 4;
+	objContext.strokeStyle = "#666";
+	objContext.stroke();
+	objContext.restore();
+
+	// Sekunden
+	objContext.save();
+	objContext.rotate(intSek * Math.PI / 30);
+	objContext.beginPath();
+	objContext.moveTo(0, 10);
+	objContext.lineTo(0, -50);
+	objContext.lineWidth = 2;
+	objContext.strokeStyle = "#a00";
+	objContext.stroke();
+	objContext.restore();
+
+	objContext.restore();
+
+	hTimer = window.setTimeout(function() {
+		AnalogClock(objCanvas);
+	}, 1000);
+}
---
+++ Yet-Another-Calendar/src/main/webapp/Resources/calendar_monthview.xsl
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="ISO-8859-1"?>
+<xsl:stylesheet version="1.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:output method="html" doctype-system="http://www.w3.org/TR/html4/strict.dtd" doctype-public="-//W3C//DTD HTML 4.01//EN" indent="yes" />
+
+<xsl:variable name="cur"> </xsl:variable>
+<xsl:variable name="no" select='count(//appointment)' />
+
+<xsl:template match="/">
+	<html xmlns="http://www.w3.org/1999/xhtml">
+		<head>
+			<title>Yet Another Calendar</title>
+			<link rel="stylesheet" media="screen" href="Resources/structure.css"></link>
+			<script type="text/JavaScript" src="Resources/analog_clock.js"></script> 
+		</head>
+		<body>
+			<div id="main">
+				<div id="logo"><img src="Resources/BabyGnu.png" width="100px" height="100px" alt="logo"/></div>
+				<div id="info"><canvas width="150" height="150" id="analog_clock"></canvas></div>
+				<div id="title"><h1>Yet Another Calendar</h1></div>
+				<div id="menubar"></div>
+				<div id="calendar">
+					<xsl:apply-templates />
+				</div>
+				<div id="footer"></div>
+			</div>
+		</body>
+	</html>
+</xsl:template>
+
+<xsl:template match="calendar">
+	<ul id="year">
+      <xsl:for-each select="year/month">
+		<li class="month">
+		<ul class="daylist">
+			<xsl:variable name="cur" select="@name"></xsl:variable>
+			<li class="monthtitle"><xsl:value-of select="$cur"/></li>
+			<xsl:apply-templates/>
+		</ul>
+		</li>
+      </xsl:for-each>
+    </ul>
+</xsl:template>
+
+<xsl:template match="day">
+	<li class="day">
+	<xsl:variable name="curd" select="@number"> </xsl:variable>
+	<div class="daylabel"><xsl:value-of select="$curd"/> <xsl:text> </xsl:text> <xsl:value-of select="@name"/></div>
+	
+	<xsl:variable name="no" select='count(entry)' />
+	<xsl:variable name="height" select='days * 23 - 3' /> 
+	<xsl:if test="$no = 1">
+	<div class="entry" title="{desc}" ><xsl:value-of select="$no"/><xsl:text> Term.</xsl:text></div>
+	</xsl:if>
+	<xsl:if test="$no &gt; 1">
+	<div class="entry" title="{desc}"><xsl:value-of select="$no"/><xsl:text> Term</xsl:text></div>
+	</xsl:if>
+		
+	</li>
+</xsl:template>
+
+<xsl:template match="entry">
+</xsl:template>
+
+</xsl:stylesheet>
