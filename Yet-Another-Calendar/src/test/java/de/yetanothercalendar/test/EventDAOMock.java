de3f66d1de52eb244f8bb74dd01ae66ae1d9fc48
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2012-06-05 14:10:10 +0200

    Add EventDAOMock for test case

---
+++ Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/EventDAOMock.java
@@ -0,0 +1,34 @@
+package de.yetanothercalendar.test;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
+
+import de.yetanothercalendar.model.dao.EventDAO;
+import de.yetanothercalendar.model.database.Event;
+import de.yetanothercalendar.model.database.User;
+
+public class EventDAOMock implements EventDAO {
+
+	public void createUserTable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Event> getEventBetweenDates(User user, Date from, Date til) {
+		List<Event> eventList = new ArrayList<Event>();
+		Calendar created = new GregorianCalendar(Locale.GERMANY);
+		created.set(2012, 03, 06, 10, 00);
+		Calendar start = new GregorianCalendar(Locale.GERMANY);
+		start.set(2012, 03, 06, 11, 00);
+		Calendar end = new GregorianCalendar(Locale.GERMANY);
+		start.set(2012, 03, 06, 13, 00);
+		eventList.add(new Event(null, null, null, null, start.getTime(),
+				created.getTime(), null, null, null, null, null, null, null,
+				end.getTime(), 0, null, null, null, null, null));
+		return eventList;
+	}
+}
