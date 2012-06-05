0f06e06cc4c530c9c2a3b8101f98605e318fe069
Jan B <jabro91@gmx.de>
2012-06-05 12:40:00 +0200

    Ical4J eingeführt, DateParsing (16) funktioniert

--- Yet-Another-Calendar/src/main/java/de/yetanothercalendar/model/iCal/ICalendarParser.java
+++ Yet-Another-Calendar/src/main/java/de/yetanothercalendar/model/iCal/ICalendarParser.java
@@ -1,10 +1,15 @@
 package de.yetanothercalendar.model.iCal;
 
-import java.awt.Event;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.ListIterator;
 
-import de.yetanothercalendar.model.calendar.CalendarEntry;
+import de.yetanothercalendar.model.database.Event;
+import de.yetanothercalendar.model.database.User;
 
 public class ICalendarParser {
 
@@ -21,7 +26,6 @@ public class ICalendarParser {
 		int eventBeginPos = calendarString.indexOf(EVENT_BEGIN);
 		int eventEndPos = calendarString.indexOf(EVENT_END);
 
-		String debug = calendarString.substring(0, EVENT_BEGIN.length());
 		while ((eventBeginPos != -1) && (eventEndPos != -1)) {
 			// Maybe replace by if calendarString.startsWith(EVENT_BEGIN),
 			// because after an End must be a start --> faster? Problem with Space, Line Breaks --> this is safer ;-)
@@ -35,6 +39,97 @@ public class ICalendarParser {
 			eventEndPos = calendarString.indexOf(EVENT_END);
 		}
 		return events;
+	}
+	
+	public static List<Event> parseEvents(List<String> events) throws ParseException{
+		List<Event> eventList = new ArrayList<Event>();
+		
+		int begin;
+		final int DATE_LENGTH = 15;
+		Event tmpEvent;
+		Long id;				//What to set here?
+		User user;				//Need this from somewhere --> Import Parameter?
+		Date dtstamp;
+		String uid;
+		Date dtstart;
+		Date created;
+		String description;
+		Date lastmod;
+		String location;
+		String priority;
+		String summary;
+		String recurid;
+		String rrule;
+		Date dtend;
+		long duration;
+		String color;
+		List<String> categories;
+		String comment;
+		Date exdate;
+		Date rdate;
+		
+		/**
+		 * 	private Long id;					//What to set here?
+		 *  private User user;					//Need this from somewhere --> Import Parameter?
+		 * 	private Date dtstamp;
+		 * 	private String uid;
+		 * 	private Date dtstart;
+		 * 	private Date created;
+		 * 	private String description;
+		 * 	private Date lastmod;
+		 * 	private String location;
+		 * 	private String priority;
+		 * 	private String summary;
+		 * 	private String recurid;
+		 * 	private String rrule;
+		 * 	private Date dtend;
+		 * 	private long duration;
+		 * 	private String color;				//Need this as import parameter
+		 * 	private List<String> categories;
+		 * 	private String comment;
+		 * 	private Date exdate;
+		 * 	private Date rdate;
+		 */
+		
+		ListIterator<String> it = events.listIterator(events.size());
+		while(it.hasNext()){
+			dtstamp = parseIcsDate(getEventItem("DTSTAMP:", DATE_LENGTH, it.next()));
+				
+//			uid;
+//			dtstart;
+//			created;
+//			description;
+//			lastmod;
+//			location;
+//			priority;
+//			summary;
+//			recurid;
+//			rrule;
+//			dtend;
+//			duration;
+//			color;
+//			List<String> categories;
+//			comment;
+//			exdate;
+//			rdate;
+		}
+		
+		return eventList;
+	}
+	
+	public static Date parseIcsDate (String dateString) throws ParseException{
+		dateString = dateString.replace('T', ' ');
+		DateFormat df = new SimpleDateFormat ("yyyyMMdd HHmmss");
+		Date date = df.parse(dateString);
+		System.out.println(date.toString());
+		return date;
+	}
+	public static String getEventItem (String name, int itemLength, String event){
+		String eventItem;
+		int itemStart;
+		itemStart = event.indexOf(name);
+		eventItem = event.substring(itemStart+name.length()+1, itemStart+name.length()+1+itemLength);
+		return eventItem;
 	}
 }
 
