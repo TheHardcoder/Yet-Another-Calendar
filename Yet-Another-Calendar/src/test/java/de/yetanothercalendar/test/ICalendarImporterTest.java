6f0f466c8cdb18f5aca1fc01fd3e52c014f7c364
Jan B <jabro91@gmx.de>
2012-06-06 17:33:32 +0200

    kleinere Änderungen und Korrektur am iCal TestCase

--- Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/ICalendarImporterTest.java
+++ Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/ICalendarImporterTest.java
@@ -17,26 +17,9 @@ import de.yetanothercalendar.model.iCal.ICalendarImporter;
 
 public class ICalendarImporterTest extends TestCase {
 
-	 @Test
-	 public void TesParseIcal4JToEventList(){
-			File f = new File("simple/complex.ics");
-			InputStream in = null;
-			try {
-				in = new FileInputStream(f);
-				Calendar test = ICalendarImporter.importToIcal4J(in);
-				List<Event> events = new ArrayList();
-				User user = new User("test@test.de", "test", "test", "123456");
-				events = ICalendarImporter.parseIcal4JToEventList(test, user);
-				} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				this.fail("Error parsing sample .ics File");
-			}
-		}
-
 	@Test
 	public void testImportToIcal4J() {
-		File f = new File("resources/complex.ics");
+		File f = new File("resources/simple.ics");
 		InputStream in = null;
 		try {
 			in = new FileInputStream(f);
