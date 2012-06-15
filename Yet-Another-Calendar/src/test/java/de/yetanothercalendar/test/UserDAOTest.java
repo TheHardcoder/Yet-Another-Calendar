3762164067e7e3fef2bd7b4feaa2746ba555e9b1
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2012-06-15 21:14:43 +0200

    Testcase rafael korrigiert. Falsche Signtur wurde verwendet

--- Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/UserDAOTest.java
+++ Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/UserDAOTest.java
@@ -9,48 +9,45 @@ import de.yetanothercalendar.model.database.helper.DatabaseConnectionManager;
 import junit.framework.TestCase;
 
 public class UserDAOTest extends TestCase {
-	
-/*
-	public void testCreateUserTable(){
-		
-		UserDAOImpl user = new UserDAOImpl(new DatabaseConnectionManager("admin", "admin", 
-				"localhost", 3306, "yetanothercalendar"));
-		assertNotNull("Anlegen des UserDAOs fehlgeschlagen!", user);
-		user.createUserTable();		
-	}
-*/
+
+	/*
+	 * public void testCreateUserTable(){
+	 * 
+	 * UserDAOImpl user = new UserDAOImpl(new DatabaseConnectionManager("admin",
+	 * "admin", "localhost", 3306, "yetanothercalendar"));
+	 * assertNotNull("Anlegen des UserDAOs fehlgeschlagen!", user);
+	 * user.createUserTable(); }
+	 */
 	@Test
-	public void testCreateUser(){
-		UserDAOImpl user = new UserDAOImpl(new DatabaseConnectionManager("admin", "admin", 
-				"localhost", 3306, "yetanothercalendar"));
-		
-		if(user.createUser("zeller6@yahoo.de", "Paull", "Sulzer", "test")==true){
+	public void testCreateUser() {
+		UserDAOImpl user = new UserDAOImpl(new DatabaseConnectionManager(
+				"admin", "admin", "localhost", 3306, "yetanothercalendar"));
+		// FIXME - Bitte Signatur korrigieren
+		User userObj = new User("zeller6@yahoo.de", "Paull", "Sulzer", "test");
+		if (user.createUser(userObj)) {
 			System.out.println("Hat funktioniert");
-		}
-		else{
-			System.out.println("hat nicht funktioniert, User existiert wahrscheinlich ");
+		} else {
+			System.out
+					.println("hat nicht funktioniert, User existiert wahrscheinlich ");
 		}
 	}
-	
+
 	@Test
-	public void testReturnUser(){
-		UserDAOImpl userImp = new UserDAOImpl(new DatabaseConnectionManager("admin", "admin", 
-				"localhost", 3306, "yetanothercalendar"));
-		
-		if(userImp.checkUser("zeller@yahoo.de", "Test")){
-			User user =userImp.returnUser("zeller@yahoo.de");
-			System.out.println("Email: " + user.getEmail() + " Vorname: " + 
-								user.getForename() + " ID: " + user.getId() +
-								" Nachname: " + user.getLastname() + 
-								" Passwort: " +user.getPasswordSHA1());
+	public void testReturnUser() {
+		UserDAOImpl userImp = new UserDAOImpl(new DatabaseConnectionManager(
+				"admin", "admin", "localhost", 3306, "yetanothercalendar"));
+
+		if (userImp.checkUser("zeller@yahoo.de", "Test")) {
+			User user = userImp.returnUser("zeller@yahoo.de");
+			System.out.println("Email: " + user.getEmail() + " Vorname: "
+					+ user.getForename() + " ID: " + user.getId()
+					+ " Nachname: " + user.getLastname() + " Passwort: "
+					+ user.getPasswordSHA1());
+		} else {
+			System.out.println("Email-Adresse oder Passwort sind falsch! "
+					+ "Bitte ï¿½berprï¿½fen");
 		}
-		else{
-			System.out.println("Email-Adresse oder Passwort sind falsch! " +
-					"Bitte überprüfen");
-		}
-		
-		
+
 	}
-		
 
 }
