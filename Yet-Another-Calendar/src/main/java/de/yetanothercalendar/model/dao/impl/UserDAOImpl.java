de61e836f6454306ae9481b84ed3fb18882fea96
Rafael Zeller <zellerrafael@gmail.com>
2012-06-15 14:22:15 +0200

    Klasse UserDAOImpl createUser, checkUser und returnUser geschrieben

--- Yet-Another-Calendar/src/main/java/de/yetanothercalendar/model/dao/impl/UserDAOImpl.java
+++ Yet-Another-Calendar/src/main/java/de/yetanothercalendar/model/dao/impl/UserDAOImpl.java
@@ -1,13 +1,14 @@
 package de.yetanothercalendar.model.dao.impl;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import de.yetanothercalendar.model.dao.UserDAO;
+import de.yetanothercalendar.model.database.User;
 import de.yetanothercalendar.model.database.helper.DatabaseConnectionManager;
-
 
 /**
  * Ãœber die Klasse {@link UserDAOImpl} erfolgt der Zugriff auf die Datenbank
@@ -20,7 +21,7 @@ public class UserDAOImpl implements UserDAO {
 	public UserDAOImpl(DatabaseConnectionManager manager) {
 		super();
 		this.manager = manager;
-		
+
 	}
 
 	public void createUserTable() {
@@ -39,39 +40,105 @@ public class UserDAOImpl implements UserDAO {
 			ex.printStackTrace();
 		}
 	}
-	
-	public boolean createUser(String email, String forename, String lastname, String password){
-		try{		
-		Connection con = manager.getConnection();
-		Statement createStatement = con.createStatement();
-		
-		ResultSet rsUsers = createStatement.executeQuery("SELECT email From users"
-							+ "WHERE email = \" " + email + "\" ;");
-		String dbEmail = rsUsers.getString(0);
-		
-		if(dbEmail.equalsIgnoreCase(email)){
+
+	public boolean createUser(String email, String forename, String lastname,
+			String password) {
+		try {
+			Connection con = manager.getConnection();
+			Statement createStatement = con.createStatement();
+			String userSurchString = "select email from users where email = \""
+					+ email + "\";";
+
+			ResultSet rsUsers = createStatement.executeQuery(userSurchString);
+			String dbEmail = "";
+
+			while (rsUsers.next()) {
+				dbEmail = rsUsers.getString(1);
+			}
+
+			rsUsers.close();
+			if (dbEmail.equalsIgnoreCase(email)) {
+				// Wenn die Mail-Adresse schon existiert wird false
+				// zurückgegeben
+				return false;
+			} else {
+				String usercreationString = "INSERT INTO users "
+						+ "(email, forename, lastname, password)"
+						+ "VALUES (\"" + email + "\", \" " + forename
+						+ "\", \" " + lastname + "\", \" " + password + "\");";
+
+				createStatement.executeUpdate(usercreationString);
+
+				createStatement.close();
+				con.close();
+				return true;
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+
 			return false;
 		}
-		else{
-			
-		
-			String usercreationString = "INSERT INTO users "
-					+ "(email, forename, lastname, password)"
-					+ "VALUES (\" " + email + "\", \" " + forename 
-					+ "\", \" " + lastname + "\", \" " + password +");";
-		
-			createStatement.executeUpdate(usercreationString);
-		
-			createStatement.close();
-			return true;
+
+	}
+
+	public boolean checkUser(String email, String password) {
+		try {
+			Connection con = manager.getConnection();
+			Statement createStatement = con.createStatement();
+			String userSurch = "select password from users where email = \""
+					+ email + "\";";
+			ResultSet rsUser = createStatement.executeQuery(userSurch);
+			String dbPassword;
+			while (rsUser.next()) {
+				dbPassword = rsUser.getString(1);
+				System.out.println(dbPassword +"= " + password);
+				if (dbPassword.equals(password)) {
+					createStatement.close();
+					con.close();
+					return true;
+				} else {
+					createStatement.close();
+					con.close();
+					return false;
+				}
 			}
-		}catch (SQLException e){
+			createStatement.close();
+			con.close();
+			return false;
+
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return false;
 		}
-		
+
 	}
-	
-	
+
+	public User returnUser(String email) {
+		try {
+			Connection con = manager.getConnection();
+			Statement createStatement = con.createStatement();
+			String userSurch = "select * from users where email = \"" + email
+					+ "\";";
+
+			ResultSet rsUsers = createStatement.executeQuery(userSurch);
+
+			rsUsers.next();
+
+			Long dbId = rsUsers.getLong(1);
+			String dbEmail = rsUsers.getString(2);
+			String dbForename = rsUsers.getString(3);
+			String dbLastname = rsUsers.getString(4);
+			String dbPassword = rsUsers.getString(5);
+
+			createStatement.close();
+			con.close();
+			return new User(dbId, dbEmail, dbForename, dbLastname, dbPassword);
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
