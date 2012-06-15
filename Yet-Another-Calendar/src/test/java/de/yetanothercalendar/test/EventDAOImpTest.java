d602f61c3d1bd6f530ec49011c93618d5ac94ee0
Rafael Zeller <zellerrafael@gmail.com>
2012-06-15 17:59:29 +0200

    EventDAOImpTest hinzugefuegt

---
+++ Yet-Another-Calendar/src/test/java/de/yetanothercalendar/test/EventDAOImpTest.java
@@ -0,0 +1,21 @@
+package de.yetanothercalendar.test;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import de.yetanothercalendar.model.dao.impl.EventDAOImpl;
+import de.yetanothercalendar.model.dao.impl.UserDAOImpl;
+import de.yetanothercalendar.model.database.Event;
+import de.yetanothercalendar.model.database.helper.DatabaseConnectionManager;
+
+public class EventDAOImpTest extends TestCase {
+	
+	@Test
+	public void testCreateUserTable() {
+
+		EventDAOImpl user = new EventDAOImpl(new DatabaseConnectionManager(
+				"admin", "admin", "localhost", 3306, "yetanothercalendar"));
+		assertNotNull("Anlegen des UserDAOs fehlgeschlagen!", user);
+		user.createEventTable();
+	}
+
+}
