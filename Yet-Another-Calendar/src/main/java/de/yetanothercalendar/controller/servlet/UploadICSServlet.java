5ac68027eacdd7f902029ca9bec210f438a7fd77
Fabian Schwarz-Fritz <fabian@schwarz-fritz.de>
2012-07-05 20:54:38 +0200

    Add server download

--- Yet-Another-Calendar/src/main/java/de/yetanothercalendar/controller/servlet/UploadICSServlet.java
+++ Yet-Another-Calendar/src/main/java/de/yetanothercalendar/controller/servlet/UploadICSServlet.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -24,6 +25,7 @@ import de.yetanothercalendar.model.dao.impl.EventDAOImpl;
 import de.yetanothercalendar.model.database.Event;
 import de.yetanothercalendar.model.database.User;
 import de.yetanothercalendar.model.database.helper.DatabaseConnectionManager;
+import de.yetanothercalendar.model.iCal.ICalendarExporter;
 import de.yetanothercalendar.model.iCal.ICalendarImporter;
 
 public class UploadICSServlet extends HttpServlet {
@@ -32,56 +34,93 @@ public class UploadICSServlet extends HttpServlet {
 
 	public UploadICSServlet() {
 		dao = new EventDAOImpl(new DatabaseConnectionManager());
-		;
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// response.setContentType("text/html");
-		// PrintWriter out = response.getWriter();
+		String parameter = request.getParameter("action");
 		User user = (User) request.getSession().getAttribute("user");
 		if (user != null) {
-			boolean isMultipartContent = ServletFileUpload
-					.isMultipartContent(request);
-			if (!isMultipartContent) {
-				throw new RuntimeException("DAs ist kein file upload");
-			}
-			FileItemFactory factory = new DiskFileItemFactory();
-			ServletFileUpload upload = new ServletFileUpload(factory);
-			try {
-				List<FileItem> fields = upload.parseRequest(request);
-				Iterator<FileItem> it = fields.iterator();
-				if (!it.hasNext()) {
-					throw new RuntimeException("Kein File item");
+			if (parameter.equals("import")) {
+				boolean isMultipartContent = ServletFileUpload
+						.isMultipartContent(request);
+				if (!isMultipartContent) {
+					throw new RuntimeException("DAs ist kein file upload");
 				}
-				while (it.hasNext()) {
-					FileItem fileItem = it.next();
-					boolean isFormField = fileItem.isFormField();
-					if (!isFormField) {
-						InputStream inputStream = fileItem.getInputStream();
-						ICalendarImporter importer = new ICalendarImporter();
-						try {
-							Calendar importToIcal4J = importer
-									.importToIcal4J(inputStream);
-							List<Event> parseIcal4JToEventList = importer
-									.parseIcal4JToEventList(importToIcal4J,
-											user);
-							for (Event event : parseIcal4JToEventList) {
-								System.out.println("Event imported: "
-										+ event.toString());
-								dao.createEvents(event);
+				FileItemFactory factory = new DiskFileItemFactory();
+				ServletFileUpload upload = new ServletFileUpload(factory);
+				try {
+					List<FileItem> fields = upload.parseRequest(request);
+					Iterator<FileItem> it = fields.iterator();
+					if (!it.hasNext()) {
+						throw new RuntimeException("Kein File item");
+					}
+					while (it.hasNext()) {
+						FileItem fileItem = it.next();
+						boolean isFormField = fileItem.isFormField();
+						if (!isFormField) {
+							InputStream inputStream = fileItem.getInputStream();
+							ICalendarImporter importer = new ICalendarImporter();
+							try {
+								Calendar importToIcal4J = importer
+										.importToIcal4J(inputStream);
+								List<Event> parseIcal4JToEventList = importer
+										.parseIcal4JToEventList(importToIcal4J,
+												user);
+								for (Event event : parseIcal4JToEventList) {
+									System.out.println("Event imported: "
+											+ event.toString());
+									dao.createEvents(event);
+								}
+							} catch (ParserException e) {
+								e.printStackTrace();
 							}
-						} catch (ParserException e) {
-							e.printStackTrace();
 						}
 					}
+				} catch (FileUploadException e) {
+					e.printStackTrace();
 				}
-			} catch (FileUploadException e) {
-				e.printStackTrace();
+			} else if (parameter.equals("export")) {
+				List<Event> eventsFromUser = dao.getEventsFromUser(user);
+				ICalendarExporter exporter = new ICalendarExporter();
+				List<String> exportToIcal = exporter
+						.exportToIcal(eventsFromUser);
+				exportStringListToFile(exportToIcal, response);
 			}
 		} else {
 			throw new RuntimeException("No User logged in!");
+		}
+	}
+
+	private void exportStringListToFile(List<String> icalvalues,
+			HttpServletResponse response) {
+		try {
+			response.setContentType("text/plain");
+			String s = "";
+			for (String string : icalvalues) {
+				s += string;
+			}
+			ServletOutputStream out = response.getOutputStream();
+			try {
+				out.write(s.getBytes());
+			} catch (IOException e) {
+				e.printStackTrace();
+				streamError(response, e);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			streamError(response, e);
+		}
+	}
+
+	private void streamError(HttpServletResponse response, Exception ex) {
+		try {
+			System.out.println(ex.toString());
+			response.sendError(response.SC_INTERNAL_SERVER_ERROR,
+					"Internal error" + ex.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 }
