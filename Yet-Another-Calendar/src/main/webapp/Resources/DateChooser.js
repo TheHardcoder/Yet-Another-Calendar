a2a742d029809de9ebfc4bc2b6837f8af3604506
Ben <benni-vi@gmx.net>
2012-07-06 10:28:06 +0200

    Wochenberechnung gefixt

--- Yet-Another-Calendar/src/main/webapp/Resources/DateChooser.js
+++ Yet-Another-Calendar/src/main/webapp/Resources/DateChooser.js
@@ -46,11 +46,18 @@ window.onload = function() {
 function update() {
 	writeSelectableDays(daysInMonth(document.getElementById("month").value - 1, document.getElementById("year").value));
 	var date = new Date(document.getElementById("year").value, document.getElementById("month").value - 1, document.getElementById("day").value);
-	var onejan = new Date(date.getFullYear(),0,1);
-	var week = Math.ceil((((date - onejan) / 86400000) + onejan.getDay() + 1)/7);
-	if (week > 52)
-		week = 1;
-	document.getElementById("week").value = week;
+	Datum = date; // Anm. 1
+	DoDat=donnerstag(Datum);
+	kwjahr=DoDat.getFullYear();
+	DoKW1=donnerstag(new Date(kwjahr,0,4)); // Anm. 2
+	kw=Math.floor(1.5+(DoDat.getTime()-DoKW1.getTime())/86400000/7);
+	document.getElementById("week").value = kw;
+}
+
+function donnerstag(datum) { // Anm. 5
+	  var Do=new Date();
+	  Do.setTime(datum.getTime() + (3-((datum.getDay()+6) % 7)) * 86400000); // Anm. 3
+	  return Do;
 }
 
 function writeSelectableDays(max){
--- Yet-Another-Calendar/src/main/webapp/Resources/SmallCalendar.js
+++ Yet-Another-Calendar/src/main/webapp/Resources/SmallCalendar.js
@@ -4,12 +4,19 @@ var curdate = new Date();
 var monthnames = new Array("Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember");
 
 Date.prototype.getWeek = function() {
-	var onejan = new Date(this.getFullYear(),0,1);
-	var week = Math.ceil((((this - onejan) / 86400000) + onejan.getDay()+1)/7);
-	if (week > 52)
-		week = 1;
-	return week;
-} 
+	Datum = this; // Anm. 1
+	DoDat=donnerstag(Datum);
+	kwjahr=DoDat.getFullYear();
+	DoKW1=donnerstag(new Date(kwjahr,0,4)); // Anm. 2
+	kw=Math.floor(1.5+(DoDat.getTime()-DoKW1.getTime())/86400000/7);
+	return kw;
+}
+
+function donnerstag(datum) { // Anm. 5
+	  var Do=new Date();
+	  Do.setTime(datum.getTime() + (3-((datum.getDay()+6) % 7)) * 86400000); // Anm. 3
+	  return Do;
+}
 
 function create() {
 	startdate = new Date();
